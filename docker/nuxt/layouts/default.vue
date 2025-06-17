90245b96bf2f02f10addf5d6996d39e2abadd8d0
niftus13 <niftus13@gmail.com>
2025-06-17 18:59:03 +0900

    11

--- docker/nuxt/layouts/default.vue
+++ docker/nuxt/layouts/default.vue
@@ -11,6 +11,8 @@
   <script setup>
   import AppHeader from '~/components/AppHeader.vue'
   import AppFooter from '~/components/AppFooter.vue'
+
+  
   </script>
 
 <style>
--- docker/nuxt/pages/index.vue
+++ docker/nuxt/pages/index.vue
@@ -6,6 +6,9 @@
     </div>
 
 
+    
+
+
 </template>
 
 <script setup>
