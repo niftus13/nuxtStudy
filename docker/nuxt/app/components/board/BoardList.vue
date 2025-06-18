38ef78837236b5f9dc5b27014ddc57548f4d7be9
niftus13 <niftus13@gmail.com>
2025-06-18 16:37:17 +0900

    11

--- docker/nuxt/app/components/board/BoardList.vue
+++ docker/nuxt/app/components/board/BoardList.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <ul v-if="posts.length">
+        <ul v-if="posts?.length">
             <li v-for="post in posts" :key="post.id">
                 <NuxtLink :to="`/board/edit/${post.id}`">
                     {{ post.title }}
                 </NuxtLink>
             </li>
         </ul>
-
+        <p v-else>게시글이 없습니다.</p>
     </div>
 </template>
 
